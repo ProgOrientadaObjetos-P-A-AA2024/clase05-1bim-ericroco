9b20d8b041e787b84509fa7baabb376cb14ad733
ericroco <erickrodas559@gmail.com>
2024-05-15 15:56:15 -0500

    clase05

---
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete04/Ejecutor2w.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete04;
+
+import paquete01.LibretaCalificacion;
+/**
+ *
+ * @author utpl
+ */
+public class Ejecutor2w {
+
+    public static void main(String[] args) {
+        double[] notas = {8,9,10,6};
+        String nombre = "Erick Rodas";
+        LibretaCalificacion l2 = new LibretaCalificacion(nombre, notas);
+    }
+}
